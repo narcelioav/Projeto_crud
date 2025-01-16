b96a886354a6a9ce52a7927661e647107d2bfaa4
narcelioav <narcelioav@gmail.com>
2025-01-16 11:57:31 -0300

    Criando um site para derecionar a pastas com a aula expecifica.

---
+++ Javascript/aula_6/personalizado.js
@@ -0,0 +1,26 @@
+var num1 = 17;
+var num2 = 5;
+
+document.write("<p>Aula 5</p>");
+
+document.write("<h1>Operações de Atribuição</h1>");
+
+/*num1 += num2;
+//num1 = num1 + num2;
+document.write("Operação de Atribuição com adição: " + num1 + "<br>");
+
+num1 -= num2;
+//num1 = num1 - num2;
+document.write("Operação de Atribuição com subtração: " + total + "<br>");
+
+num1 *= num2;
+//num1 = num1 * num2;
+document.write("Operação de Atribuição com multiplicação: " + total + "<br>");
+
+num1 /= num2;
+//num1 = num1 / num2;
+document.write("Operação de Atribuição com divisão: " + total + "<br>");
+
+num1 %= num2;
+//num1 = num1 % num2;
+document.write("Operação de Atribuição com resto: " + total + "<br>");*/
