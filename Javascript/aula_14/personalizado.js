faeac89676b12191225f775279a8277f5eeb6b5f
narcelioav <narcelioav@gmail.com>
2025-02-04 00:02:39 -0300

    estudo dos comandos Do While.

---
+++ Javascript/aula_14/personalizado.js
@@ -0,0 +1,9 @@
+var total = 0;
+
+for (i = 1; i <= 5; i++) {
+	document.write("Valor da variavel i: " + i + "<br>");
+
+	total = total + 2;
+	document.write("Valor da variavel total: " + total + "<br><hr>");
+
+} 
